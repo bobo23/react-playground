22492efd07af16905bd89ae1a116a422e786f4a2
bobo23 <bosobieski@gmail.com>
2024-02-24 22:33:35 +0100

    added memory board

--- src/components/Memory.tsx
+++ src/components/Memory.tsx
@@ -1,9 +1,20 @@
 
 
 export default function Memory() {
+  const cards = [...Array(16).keys()];
+
   return (
     <div className="memory">
       <h2>Memory</h2>
+      <div className="memory__board">
+        {cards.map((card) => (
+          <div key={card} className="memory__card">
+            <div className="memory__card__inner">
+              <div className="memory__card__front">?</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
