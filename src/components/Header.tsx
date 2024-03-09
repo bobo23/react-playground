626b81c1f0e7dc039caddf23c2d6dca22efb2c10
bobo23 <bosobieski@gmail.com>
2024-03-09 23:21:58 +0100

    styling navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,28 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import logo from "../assets/react.svg";
 
 export default function Header() {
   return (
     <header className="header">
-      <h2>React Playground</h2>
+      <div className="logo">
+        <Link to="/">
+            <img src={logo}/>
+            <span>Playground</span>
+        </Link>
+      </div>
       <nav className="navbar">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link to="/">Home</Link>
+            <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/tictactoe">Tic Tac Toe</Link>
+            <NavLink to="/tictactoe" className={({ isActive }) => isActive ? "active" : ""}>Tic Tac Toe</NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/memory">Memory</Link>
+            <NavLink to="/memory" className={({ isActive }) => isActive ? "active" : ""}>Memory</NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/shopping-list">Shopping List</Link>
+            <NavLink to="/shopping-list" className={({ isActive }) => isActive ? "active" : ""}>Shopping List</NavLink>
           </li>
         </ul>
       </nav>
