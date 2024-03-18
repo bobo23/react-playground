8589aa70af009dbf4bc88da8f30af57756dc2065
bobo23 <bosobieski@gmail.com>
2024-03-18 23:08:49 +0100

    adding main image

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
 import Layout from '../Layout';
 import images from '../../data/images';
 import './Gallery.css';
 
 export default function Gallery() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const onThumbnailClick = (index: number) => {
+    setCurrentImageIndex(index);
+  }
+
   return(
     <Layout>
       <div className="gallery">
         <h2>Gallery</h2>
         <div className="gallery-container">
+        <div className="gallery-main-image-container">
+          <div className="gallery-main-image">
+            <img src={images[currentImageIndex].image} />
+          </div>
+        </div>
           <div className="gallery-thumbnail-container">
             {images.map((image, index) => (
-              <div key={index} className="gallery-thumbnail">
+              <div key={index} className="gallery-thumbnail" onClick={() => onThumbnailClick(index)}>
                 <img src={image.thumb} />
               </div>
             ))}
