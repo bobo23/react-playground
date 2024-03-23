b4a46257cb0c0a4a4f360f643791bc715da05e40
bobo23 <bosobieski@gmail.com>
2024-03-23 06:15:37 +0100

    more styling for gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faChevronLeft, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCircle as faCircleRegular } from '@fortawesome/free-regular-svg-icons';
 import Layout from '../Layout';
 import images from '../../data/images';
 import './Gallery.css';
@@ -25,24 +26,26 @@ export default function Gallery() {
   }, [currentImageIndex]);
 
   useEffect(() => {
-    const loadImages = async () => {
-      const loadedImages = await Promise.all(
-        images.map((image) =>
-          new Promise<Image>((resolve) => {
-            const img = new Image();
-            img.src = image.thumb;
-            img.onload = () => {
-              const orientation = img.naturalWidth > img.naturalHeight ? 'landscape' : 'portrait';
-              resolve({ ...image, orientation });
-            };
-          })
-        )
-      );
-      setImagesWithOrientation(loadedImages);
-    };
-  
     loadImages();
   }, []);
+
+  const loadImages = async () => {
+    const loadedImages = await Promise.all(
+      images.map((image) =>
+        new Promise<Image>((resolve) => {
+          const img = new Image();
+
+          img.src = image.thumb;
+          img.onload = () => {
+            const orientation = img.naturalWidth > img.naturalHeight ? 'landscape' : 'portrait';
+            
+            resolve({ ...image, orientation });
+          };
+        })
+      )
+    );
+    setImagesWithOrientation(loadedImages);
+  };
 
   const changeImage = (newIndex: number) => {
     let finalIndex = newIndex;
@@ -68,21 +71,31 @@ export default function Gallery() {
       <div className="gallery">
         <h2>Gallery</h2>
         <div className="gallery-container">
-        <div className="gallery-main-image-container">
-          <div className="gallery-main-image">
-            {imagesWithOrientation.length > 0 ? (
-              <img
-                key={`image_` + currentImageIndex}
-                src={imagesWithOrientation[currentImageIndex]?.image}
-                className={`${isVisible ? 'image-visible' : ''} ${imagesWithOrientation[currentImageIndex]?.orientation}`}
-              />
-            ) : (
-              <div>Loading...</div>
-            )}
-            <FontAwesomeIcon icon={faChevronLeft} className="gallery-main-image-nav-left" onClick={() => changeImage(currentImageIndex - 1)} />
-            <FontAwesomeIcon icon={faChevronRight} className="gallery-main-image-nav-right" onClick={() => changeImage(currentImageIndex + 1)} />
+          <div className="gallery-main-image-container">
+            <div className="gallery-main-image">
+              {imagesWithOrientation.length > 0 ? (
+                <img
+                  key={`image_` + currentImageIndex}
+                  src={imagesWithOrientation[currentImageIndex]?.image}
+                  className={`${isVisible ? 'image-visible' : ''} ${imagesWithOrientation[currentImageIndex]?.orientation}`}
+                />
+              ) : (
+                <div>Loading...</div>
+              )}
+              <FontAwesomeIcon icon={faChevronLeft} className="gallery-main-image-nav-left" onClick={() => changeImage(currentImageIndex - 1)} />
+              <FontAwesomeIcon icon={faChevronRight} className="gallery-main-image-nav-right" onClick={() => changeImage(currentImageIndex + 1)} />
+            </div>
+            <div className="bullets">
+              {imagesWithOrientation.map((_, index) => (
+                <FontAwesomeIcon
+                  icon={index === currentImageIndex ? faCircle : faCircleRegular}               
+                  key={index}
+                  className={`bullet ${index === currentImageIndex ? 'active' : ''}`}
+                  onClick={() => changeImage(index)}
+                />
+              ))}
+            </div>
           </div>
-        </div>
           <div className="gallery-thumbnail-container" ref={thumbnailContainerRef}>
             {imagesWithOrientation.map((image, index) => (
               <div key={index} className="gallery-thumbnail" onClick={() => changeImage(index)}>
